d5d074429250ec49e41401bfaf70731f4b10f640
Anix <jesus_heaven117@outlook.com>
2020-02-02 23:19:54 -0600

    Last changes

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import {
+    Component,
+    OnInit,
+    DoCheck,
+    AfterContentInit,
+    AfterContentChecked,
+    AfterViewInit,
+    AfterViewChecked,
+    OnDestroy,
+} from '@angular/core';
 
 @Component({
     selector: 'app-home',
@@ -22,8 +31,48 @@ import { Component, OnInit } from '@angular/core';
     `,
     styles: [],
 })
-export class HomeComponent implements OnInit {
-    constructor() {}
+export class HomeComponent
+    implements
+        OnInit,
+        DoCheck,
+        AfterContentInit,
+        AfterContentChecked,
+        AfterViewInit,
+        AfterViewChecked,
+        OnDestroy {
+    constructor() {
+        console.log('constructor');
+    }
 
-    ngOnInit() {}
+    ngOnInit() {
+        console.log('ngOnInit');
+    }
+
+    ngOnChanges() {
+        console.log('ngOnChanges');
+    }
+
+    ngDoCheck() {
+        console.log('ngDoCheck');
+    }
+
+    ngAfterContentInit() {
+        console.log('ngAfterContentInit');
+    }
+
+    ngAfterContentChecked() {
+        console.log('ngAfterContentChecked');
+    }
+
+    ngAfterViewInit() {
+        console.log('ngAfterViewInit');
+    }
+
+    ngAfterViewChecked() {
+        console.log('ngAfterViewChecked');
+    }
+
+    ngOnDestroy() {
+        console.log('ngOnDestroy');
+    }
 }
--- src/app/components/usuario/usuario-nuevo.component.ts
+++ src/app/components/usuario/usuario-nuevo.component.ts
@@ -1,19 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
-  selector: 'app-usuario-nuevo',
-  template: `
-    <p>
-      usuario-nuevo works!
-    </p>
-  `,
-  styles: []
+    selector: 'app-usuario-nuevo',
+    template: `
+        <p>
+            usuario-nuevo works!
+        </p>
+    `,
+    styles: [],
 })
 export class UsuarioNuevoComponent implements OnInit {
+    constructor(private router: ActivatedRoute) {
+        this.router.parent.params.subscribe(parametros => {
+            console.log('RUTA HIJA');
+            console.log(parametros);
+        });
+    }
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+    ngOnInit() {}
 }
